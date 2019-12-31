76128163f593e80a6bcedb94f7e3d7432d25e544
lushe <lushe.yc@gmail.com>
2019-12-31 14:01:04 +0800

    init

---
+++ tsd-core/src/main/java/com/lushe/tsd/core/entity/Task.java
@@ -0,0 +1,14 @@
+package com.lushe.tsd.core.entity;
+
+/**
+ * @author lushe
+ * @date 2019/12/31 1:58 PM
+ * @Des:
+ **/
+public class Task {
+
+    private Integer taskId;
+
+    private String taskName;
+
+}
